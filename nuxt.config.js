d60c28ca6cfa6c944bbd83dc347a01b1d80c4d82
Mario Kaiser <ehmprah@gmail.com>
2019-11-09 16:58:25 +0100

    Setup and first steps

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+/*
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#26a7ce">
+<meta name="msapplication-TileColor" content="#000000">
+<meta name="theme-color" content="#ffffff"></meta>
+*/
+
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    /* favicons */
+    // TODO add them
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
---
+++ pages/imprint.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="imprint">
+      <h1 class="imprint__heading">Imprint</h1>
+      <div>
+        <p>
+          Mario Kaiser<br />
+          Graudenzer Str. 17<br />10243 Berlin
+        </p>
+        <p>Email: <a href="mailto:ehmprah@gmail.com">ehmprah@gmail.com</a></p>
+      </div>
+      <a href="/">Back</a>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+.imprint {
+  width: 80vw;
+  max-width: 480px;
+  margin: 3em auto;
+  &__heading {
+    font-size: 2em;
+    line-height: 1.5em;
+    color: #26a7ce;
+    margin-bottom: 0.5em;
+  }
+}
+</style>
